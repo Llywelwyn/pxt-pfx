955a5a5c8d9c1d8fa5026b871534edd83ea7da8a
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-03-26 17:38:55 +0000

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://llywelwyn.github.io/tcz-particles/](https://llywelwyn.github.io/tcz-particles/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [/](/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/llywelwyn/tcz-particles** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [/](/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/llywelwyn/tcz-particles** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,110 @@
+game.currentScene().physicsEngine = new ArcadePhysicsEngine(10000)
+
+interface ParticleConfig {
+    minSize?: number;
+    maxSize?: number;
+    speed?: number;
+    speedRange?: boolean;
+    lifespan?: number;
+    lifespanRange?: boolean;
+    bouncy?: boolean;
+    direction?: number;
+    angle?: number;
+}
+
+class ParticlePresets {
+    static readonly circle: ParticleConfig = { lifespanRange: true, speedRange: true };
+    static readonly ring: ParticleConfig = { lifespanRange: true, speedRange: false }
+}
+
+class ParticleEffect {
+    numOfParticles: number;
+    minSize: number;
+    maxSize: number;
+    speed: number;
+    speedRange: boolean;
+    lifespan: number;
+    lifespanRange: boolean;
+    bouncy: boolean;
+    direction: number;
+    angle: number;
+
+    static readonly RADIANS_IN_A_DEGREE: number = Math.PI / 180
+
+    constructor(n: number, config:  ParticleConfig = {}) {
+        this.numOfParticles = n;
+        this.setConfig(config);
+    }
+
+    public setConfig(config: ParticleConfig) {
+        this.minSize = config.minSize ? config.minSize : 1;
+        this.maxSize = config.maxSize ? config.maxSize : 4;
+        this.speed = config.speed ? config.speed : 100;
+        this.speedRange = config.speedRange ? config.speedRange : false;
+        this.lifespan = config.lifespan ? config.lifespan : 1000;
+        this.lifespanRange = config.lifespanRange ? config.lifespanRange : false;
+        this.bouncy = config.bouncy ? config.bouncy : true;
+        this.direction = config.direction ? config.direction : 0;
+        this.angle = config.angle ? config.angle : 2 * Math.PI;
+    }
+
+    public setDirection(n: number, usingRadians: boolean = false) {
+        this.direction = usingRadians? n : n * ParticleEffect.RADIANS_IN_A_DEGREE
+    }
+
+    public setAngle(n: number, usingRadians: boolean = false) {
+        this.angle = usingRadians ? n : n * ParticleEffect.RADIANS_IN_A_DEGREE
+    }
+
+    public go() {
+        for (let i = 0; i < this.numOfParticles; i++) {
+            let s = ParticleEffect.spriteOfSize(this.minSize, this.maxSize);
+            s.lifespan = this.lifespanRange ? randint(1, this.lifespan) : this.lifespan
+            this.moveAtRandAngle(s)
+            ParticleEffect.moveAtSpeed(s, this.speed, this.speedRange)
+            this.setFlags(s)
+        }
+    }
+
+    private static spriteOfSize(min: number, max: number): Sprite {
+        let s = sprites.create(image.create(
+            randint(min, max),
+            randint(min, max)
+        ))
+        s.image.fill(randint(0, 16))
+        return s
+    }
+    private setFlags(s: Sprite) {
+        s.setFlag(SpriteFlag.BounceOnWall, this.bouncy)
+        s.setFlag(SpriteFlag.AutoDestroy, true)
+    }
+    private static randAngle(dir: number = 0, max_angle: number = 2 * Math.PI): number {
+        const lower = -Math.PI/2 - max_angle/2 + dir;
+        const upper = -Math.PI/2 - max_angle/2 + max_angle + dir;
+        return randint(lower, upper)
+    }
+    private moveAtRandAngle(s: Sprite) {
+        let angle = ParticleEffect.randAngle(this.direction, this.angle)
+        s.vx = Math.cos(angle)
+        s.vy = Math.sin(angle)
+    }
+    private static moveAtSpeed(s: Sprite, speed: number, varies: boolean) {
+        s.vx *= varies ? randint(0, speed) : speed
+        s.vy *= varies ? randint(0, speed) : speed
+    }
+}
+
+class ParticleFactory {
+    fx: ParticleEffect[] = []
+
+    constructor() {}
+
+    public add(e: ParticleEffect) { this.fx.push(e); }
+    public go() { this.fx.forEach((e) => { e.go() }) }
+}
+
+let factory = new ParticleFactory;
+let e = new ParticleEffect(1000, ParticlePresets.ring)
+
+factory.add(e)
+factory.go()
