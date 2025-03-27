872d353b6e04d0e587f3a444dbb8b3bbf4470f5e
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-03-27 12:42:05 +0000

    changing colour and aiming towards sprites

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ interface ParticleConfig {
     minSize?: number;
     /** Maximum particle size in Pixels. */
     maxSize?: number;
+    /** Particle colour. */
+    color?: number;
     /** Speed of particle. */
     speed?: number;
     /** If true, speed will be randomised between 0 and speed. */
@@ -27,6 +29,7 @@ class ParticlePresets {
     static readonly circle: ParticleConfig = {
         minSize: 1,
         maxSize: 4,
+        color: -1,
         speed: 100,
         speedVaries: true,
         lifespan: 1000,
@@ -36,7 +39,28 @@ class ParticlePresets {
         spreadAngle: 2 * Math.PI,
         gravity: 0,
     };
-    static readonly ring: ParticleConfig = { lifespanVaries: true, speedVaries: false }
+    static readonly line: ParticleConfig = {
+        speedVaries: false,
+        lifespanVaries: false,
+        spreadAngle: 0,
+    }
+    static readonly ring: ParticleConfig = { 
+        lifespanVaries: true, 
+        speedVaries: false 
+    };
+    static readonly smoke: ParticleConfig = {
+        minSize: 1,
+        maxSize: 6,
+        color: 1,
+        speed: 20,
+        speedVaries: true,
+        lifespan: 3000,
+        lifespanVaries: true,
+        bouncy: false,
+        direction: 0,
+        spreadAngle: Math.PI / 2,
+        gravity: -10,
+    };
 
     static cloneConfig(config: ParticleConfig): ParticleConfig {
         return {
@@ -49,8 +73,14 @@ class ParticlePresets {
             bouncy: config.bouncy,
             direction: config.direction,
             spreadAngle: config.spreadAngle,
+            gravity: config.gravity,
         };
     }
+}
+
+interface Point {
+    x: number,
+    y: number,
 }
 
 class ParticleEffect {
@@ -58,6 +88,7 @@ class ParticleEffect {
     private numOfParticles: number;
     /** Delay before particles are emitted. */
     private delay: number = 0;
+    private pos: Point = { x: 80, y: 60 }
     /** Particle configuration. */
     config: ParticleConfig = ParticlePresets.cloneConfig(ParticlePresets.circle);
 
@@ -66,6 +97,22 @@ class ParticleEffect {
     constructor(n: number, config?: ParticleConfig) {
         this.numOfParticles = n;
         if (config) { this.setConfig(config); }
+    }
+
+    public rainbow(): ParticleEffect {
+        this.config.color = -1;
+        return this;
+    }
+
+    public setPosition(x: number, y: number): ParticleEffect {
+        this.pos.x = x;
+        this.pos.y = y;
+        return this;
+    }
+
+    public setColor(color: number): ParticleEffect {
+        this.config.color = color;
+        return this;
     }
 
     public setConfig(config: ParticleConfig) {
@@ -83,11 +130,32 @@ class ParticleEffect {
         this.config.direction = setOrDefault(config.direction, ParticlePresets.circle.direction);
         this.config.spreadAngle = setOrDefault(config.spreadAngle, ParticlePresets.circle.spreadAngle);
         this.config.gravity = setOrDefault(config.gravity, ParticlePresets.circle.gravity);
+        this.config.color = setOrDefault(config.color, ParticlePresets.circle.color);
         return this;
     }
 
     public setDirection(n: number, usingRadians: boolean = false): ParticleEffect {
-        this.config.direction = usingRadians? n : n * ParticleEffect.RADIANS_IN_A_DEGREE
+        this.config.direction = usingRadians ? n : n * ParticleEffect.RADIANS_IN_A_DEGREE
+        return this;
+    }
+
+    public addDirection(n: number, usingRadians: boolean = false): ParticleEffect {
+        let dn = usingRadians ? n : n * ParticleEffect.RADIANS_IN_A_DEGREE
+        this.config.direction += dn;
+        return this;
+    }
+
+    public aimTowards(otherSprite: Sprite): ParticleEffect {
+        /** An offset of Math.PI/2 is removed from the direction when
+         *  the emission is generated, so that 0-degrees is Upwards,
+         *  which is more intuitive than 0-degrees being Rightwards.
+         * 
+         *  We need to remove the same offset here.
+         */
+        const MINUS_OFFSET = Math.PI/2
+        let dx = otherSprite.x - this.pos.x;
+        let dy = otherSprite.y - this.pos.y;
+        this.setDirection(Math.atan2(dy, dx) + MINUS_OFFSET, true);
         return this;
     }
 
@@ -121,7 +189,12 @@ class ParticleEffect {
     public emit() {
         setTimeout(() => {
             for (let i = 0; i < this.numOfParticles; i++) {
-                let s = ParticleEffect.spriteOfSize(this.config.minSize, this.config.maxSize);
+                let s = ParticleEffect.makeParticleOfSize(
+                    this.config.minSize,
+                    this.config.maxSize
+                );
+                this.setParticleColor(s);
+                s.setPosition(this.pos.x, this.pos.y);
                 s.lifespan = this.config.lifespan
                 this.moveAtRandAngle(s)
                 ParticleEffect.moveAtSpeed(s, this.config.speed, this.config.speedVaries)
@@ -131,13 +204,16 @@ class ParticleEffect {
         }, this.delay)
     }
 
-    private static spriteOfSize(min: number, max: number): Sprite {
+    private static makeParticleOfSize(min: number, max: number): Sprite {
         let s = sprites.create(image.create(
             randint(min, max),
             randint(min, max)
         ))
-        s.image.fill(randint(0, 16))
-        return s
+        return s;
+    }
+    private setParticleColor(s: Sprite) {
+        let c = (this.config.color === -1) ? randint(1, 16) : this.config.color;
+        s.image.fill(c);
     }
     private setFlags(s: Sprite) {
         s.setFlag(SpriteFlag.BounceOnWall, this.config.bouncy)
@@ -188,6 +264,7 @@ class ParticleBuilder {
         this.fx.forEach(
             (e, i) => { setTimeout(() => e.emit(), this.delay_between * i) }
         ) 
+        return this;
     }
 }
 
@@ -195,7 +272,17 @@ class ParticleBuilder {
 let e = new ParticleEffect(10)
         .setSpreadAngle(Math.PI/8, true)
 
-let r = new ParticleEffect(100, ParticlePresets.ring).setGravity(250)
+let r = new ParticleEffect(100, ParticlePresets.line)
 
-let factory = new ParticleBuilder;
-factory.add(r, 5).setDelay(100).emit();
+let n = sprites.create(image.create(4, 4))
+n.image.fill(4)
+
+let f = new ParticleBuilder;
+setInterval(() => {
+    n.setPosition(randint(1, 160), randint(1, 120))
+    r.setPosition(randint(1, 160), randint(1, 120))
+    r.aimTowards(n)
+    r.rainbow();
+    r.setSpreadAngle(8)
+    f.add(r, 5).setDelay(1000/10).emit().clear();
+}, 1000)
